6269f72545069eb463d50e20347f77faf2de4631
Mavioux <pakmarios@gmail.com>
2020-10-28 15:35:29 +0200

    Implemented the echo function

--- src/userApplication.java
+++ src/userApplication.java
@@ -1,6 +1,7 @@
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.SourceDataLine;
+import java.io.PrintWriter;
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
@@ -8,17 +9,63 @@ import java.net.SocketException;
 
 public class userApplication {
 
-    String echo_request_code = "E0180\r";
-    String image_request_code = "M8844\r";
-    String audio_request_code = "A3157 \r";
-    String ithakicopter_request_code = "M8844\r";
+    static String echo_with_added_delay_request_code = "E8601\r";
+    static String echo_with_no_delay_request_code = "E0000\r";
+    static String image_request_code = "M8844\r";
+    static String audio_request_code = "A3157 \r";
+    static String ithakicopter_request_code = "M8844\r";
 
-    void echo(int duration) {
+    static int serverPort = 38004;
+    static byte[] hostIP = {(byte) 155, (byte) 207, 18, (byte) 208};
+    static int clientPort = 48004;
 
+    static void echo(double durationInMins, DatagramSocket s, DatagramSocket r, InetAddress hostAddress, String request_code) {
+        String echoString = "";
+        int packetCounter = 0;
+
+        byte[] txbuffer = request_code.getBytes();
+        DatagramPacket p = new DatagramPacket(txbuffer, txbuffer.length, hostAddress, serverPort);
+
+        byte[] rxbuffer = new byte[2048];
+        DatagramPacket q = new DatagramPacket(rxbuffer, rxbuffer.length);
+
+        long startTime = System.currentTimeMillis();
+
+        while (System.currentTimeMillis() - startTime < durationInMins * 60 * 1000) {
+            long requestTime = System.currentTimeMillis();
+            try{
+                s.send(p);
+                System.out.println("Sent the request");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            for (;;) {
+                System.out.println("Waiting for answer");
+                try {
+                    r.receive(q);
+                    String message = new String(rxbuffer,0,q.getLength());
+                    System.out.println(message);
+                    packetCounter++;
+                    echoString += message + ", " + requestTime + ", " + System.currentTimeMillis() + ", " +(System.currentTimeMillis() - requestTime ) + ", \n";
+                    break;
+                } catch (Exception x) {
+                    System.out.println(x);
+                }
+            }
+        }
+        System.out.println(echoString);
+        System.out.println(packetCounter);
+
+        //Save echoOutput on a csv file
+        try (PrintWriter out = new PrintWriter((request_code == echo_with_added_delay_request_code) ? "./data/echo_with_added_delay.csv" : "./data./echo_with_no_delay.csv")) {
+            out.println(echoString);
+        }catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 
     public static void main(String[] args) throws Exception {
-
         DatagramSocket s;
         try {
             s = new DatagramSocket();
@@ -26,32 +73,17 @@ public class userApplication {
             e.printStackTrace();
             return;
         }
-        String packetInfo = "E8601\r";
-        byte[] txbuffer = packetInfo.getBytes();
-        int serverPort = 38004;
-        byte[] hostIP = {(byte) 155, (byte) 207, 18, (byte) 208};
-        InetAddress hostAddress = InetAddress.getByAddress(hostIP);
-        DatagramPacket p = new DatagramPacket(txbuffer, txbuffer.length, hostAddress, serverPort);
-        s.send(p);
-        int clientPort = 48004;
         DatagramSocket r = new DatagramSocket(clientPort);
-        r.setSoTimeout(1000);
-        byte[] rxbuffer = new byte[2048];
-        DatagramPacket q = new DatagramPacket(rxbuffer, rxbuffer.length);
+        InetAddress hostAddress = InetAddress.getByAddress(hostIP);
+        r.setSoTimeout(100);
 
-        for (;;) {
-            try {
-                System.out.println(1);
-                r.receive(q);
-                String message = new String(rxbuffer,0,q.getLength());
-                System.out.println(message);
-                System.out.println(2);
-            } catch (Exception x) {
-                System.out.println(x);
-            }
-        }
-//        s.close();
-//        r.close();
+        echo(0.25, s, r, hostAddress, echo_with_added_delay_request_code);
+        echo(0.25, s, r, hostAddress, echo_with_no_delay_request_code);
+        s.close();
+        r.close();
     }
-
 }
+
+
+
+
